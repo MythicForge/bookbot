44507174187342bad83aa676ed78ad74eacb1647
MythicForge <125914417+MythicForge@users.noreply.github.com>
2025-06-03 11:24:53 -0500

    finished changes for main and stats

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from stats import get_book_wc
+from stats import *
+
+book = "books/frankenstein.txt"
 
 def main():
-    
+    ###############################
+    ## Function to open the text ##
+    ###############################
     def get_book_text(book_path):
         with open(book_path) as book:
             book_content = book.read()
         return book_content
-    current_book = get_book_text("books/frankenstein.txt")
-    get_book_wc(current_book)
+    
+    ######################
+    ## Stats for Report ##
+    ######################
+    current_book = get_book_text(book)
+    wc = get_book_wc(current_book)
+    letters = get_unique_letters(current_book)
+    
+    sorted_list = merge_split_dict(letters)
+    
+    #print(sorted_list)
+    ################################
+    ## Print Report in formatting ##
+    ################################
+    def get_book_stats(letters, word_count):
+        l_data = letters                
+    # Print Format    
+        print('============ BOOKBOT ============')    
+        print(f'Analyzing book found at {book}...')
+        print('----------- Word Count ----------')
+        print(f'Found {word_count} total words')
+        print('--------- Character Count -------')
+        for pair in l_data:
+            print(f'{pair["char"]}: {pair["num"]}')
+        print("============= END ===============")
+    get_book_stats(sorted_list, wc)
 
 main()
+
--- stats.py
+++ stats.py
@@ -2,9 +2,46 @@
 #####################################
 ## Collect word count from content ##
 #####################################
-def get_book_wc(content):
-    words = content.split()
+def get_book_wc(book):
+    words = book.split()
     wc_tracker = 0
     for count in words:
         wc_tracker += 1
-    print(f'{wc_tracker} words found in the document')
+    return wc_tracker
+
+
+
+#######################################
+## Collect letter count from content ##
+#######################################
+def get_unique_letters(book):
+    
+    cased_letters = book.lower()
+    filterd_letters = set("abcdefghijklmnopqrstuvwxyzæâêëô1234567890")
+    which_letters = {}
+    
+    for letter in cased_letters:
+        if letter in filterd_letters:
+            if letter in which_letters:
+                which_letters[letter] += 1
+            else:
+                which_letters[letter] = 1
+        
+    return which_letters
+
+
+#######################################################
+## Separate dictionary content of get_unique_letters ##
+#######################################################
+
+def merge_split_dict(dict1):
+    new_list = []
+    for lttr in dict1:
+        if lttr.isalpha():
+            new_dict = {"char": lttr, "num": dict1[lttr]}        
+            new_list.append(new_dict)
+    new_list.sort(key=lambda entry: entry["num"], reverse=True)
+    return new_list
+    
+    
+
